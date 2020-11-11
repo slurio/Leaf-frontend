cd6e8f0a75312fdd0b9ad77ef3a5497dc9013248
slurio <samantha.lurio@gmail.com>
2020-11-11 10:16:21 -0500

    removed instruction back button

--- app/screens/InstructionScreen.js
+++ app/screens/InstructionScreen.js
@@ -13,12 +13,7 @@ function InstructionScreen({navigation}) {
                 <StyledText><StepLabel>2.</StepLabel> Label Scanner needs fabric content and country of origin to generate the correct information for you! Usually both are on the front label, if not locate them on the backside or on an additional label.</StyledText>
                 <StyledText><StepLabel>3.</StepLabel> Take photo of label/labels. Make sure the camera is focused and the image is not blurry for best results.</StyledText>
                 <StyledText><StepLabel>4.</StepLabel> Name your scanned item. The more specific the better, so you can view for later use.</StyledText>
-                <StyledText><StepLabel>5.</StepLabel> Now just press submit and voila! Read up on where your garment is made, the fabric content, and care instructions.</StyledText>
-                <StyledBottomView>
-                    <StyledButtonContatiner onPress={() => navigation.goBack()}>
-                        <ButtonText>GO BACK</ButtonText>
-                    </StyledButtonContatiner>
-                </StyledBottomView>
+                <LastStyledText><StepLabel>5.</StepLabel> Now just press submit and voila! Read up on where your garment is made, the fabric content, and care instructions.</LastStyledText>
             </StyledContatiner>
         </ScrollView>
     )
@@ -32,31 +27,11 @@ const InstructionContainer = styled.View`
     margin-bottom: 22px;
 `
 
-const StyledBottomView = styled.View`
-    flex: 1;
-    justifyContent: center;
-    alignItems: center;
-`
-
-const StyledButtonContatiner = styled.TouchableOpacity`
-    background-color: transparent;
-    width: 300px;
-    height: 50px;
-    align-items: center;
-    justify-content: center;
-    margin-top: 6px;
-    background-color: black;
-`
-
-const ButtonText= styled.Text`
-    color: #fff;
-    font-family: Raleway_700Bold;
-    font-size: 18px;
-    letter-spacing: 3px;
-`
-
 const StyledContatiner = styled.View`
     margin: 30px;
+    border-bottom-width: .5px;
+    border-bottom-color: grey;
+    margin-bottom: 22px;
 `
 
 const Title = styled.Text`
@@ -75,6 +50,14 @@ const StepLabel = styled.Text`
 const StyledText = styled.Text`
     margin-top: 0px;
     margin-bottom: 20px;
+    font-family: Raleway_400Regular_Italic;
+    font-size: 18px;
+    color: #222;
+`
+
+const LastStyledText = styled.Text`
+    margin-top: 0px;
+    margin-bottom: 30px;
     font-family: Raleway_400Regular_Italic;
     font-size: 18px;
     color: #222;
